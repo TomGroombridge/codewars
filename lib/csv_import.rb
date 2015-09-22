d33f599d3e3095d1430a25afc0cbb607b3759d66
TomGroombridge <groombridgetom@gmail.com>
2015-09-22 14:36:43 +0100

    worked out compound interest

--- lib/csv_import.rb
+++ lib/csv_import.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 
 
-@amount_request = 2331
+@amount_request = 1500
 @lenders = []
 CSV.foreach("../market.csv", headers: true) do |row|
   @lenders << {'name' => row[0], 'compound_interest' => row[1], 'amount' => row[2]}
@@ -10,6 +10,7 @@ end
 
 
 @lenders = @lenders.sort_by { |hsh| hsh['compound_interest'] }
+puts @lenders
 @amount_of_each_lender = @lenders.map{|h| h["amount"] }
 
 
@@ -34,5 +35,8 @@ else
 	puts "#{@lenders_needed.count} lenders are needed for this armount requested"
 	@compound_interest = []
 	@lenders.first(@lenders_needed.count).each {|l| @compound_interest << l.values_at('compound_interest')}
-	@test = @compound_interest.flatten.map(&:to_f).inject { |sum, n| sum + n }
+
+	@total_interest = @compound_interest.flatten.map(&:to_f).inject { |sum, n| sum + n }
+	@compound_interest = (@total_interest / @lenders_needed.count)
+	puts @compound_interest
 end
