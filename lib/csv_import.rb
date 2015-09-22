82bb019ef3a95bfd63ff6248a0237f4f6fc43066
TomGroombridge <groombridgetom@gmail.com>
2015-09-22 14:32:53 +0100

    clean up

--- lib/csv_import.rb
+++ lib/csv_import.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 
 
-@amount_request = 490
+@amount_request = 2331
 @lenders = []
 CSV.foreach("../market.csv", headers: true) do |row|
   @lenders << {'name' => row[0], 'compound_interest' => row[1], 'amount' => row[2]}
@@ -13,18 +13,16 @@ end
 @amount_of_each_lender = @lenders.map{|h| h["amount"] }
 
 
-# puts @amount_of_each_lender.flatten.map(&:to_f)
-
-
-
 
 @amount = @amount_of_each_lender.flatten.map(&:to_f)
+@amount_in_bank = @amount.inject(:+)
+
+puts "These is #{@amount_in_bank} funds in the bank"
 puts @amount.inspect
 
-# if @amount_request > @amount.last
-# 	puts "sorry we can't offer you a loan"
-# else
-	puts "congratulations we can offer you a loan"
+if @amount_request > @amount_in_bank
+	puts "sorry we can't offer you a loan"
+else
 	@lenders_needed = []
 	@i = 0
 	@amount.each do |a|
@@ -32,23 +30,9 @@ puts @amount.inspect
 			@lenders_needed << @i
 			@i += a
 		end
-	# end
-
-
-
-end
-	puts @lenders_needed.count
+	end
+	puts "#{@lenders_needed.count} lenders are needed for this armount requested"
 	@compound_interest = []
 	@lenders.first(@lenders_needed.count).each {|l| @compound_interest << l.values_at('compound_interest')}
 	@test = @compound_interest.flatten.map(&:to_f).inject { |sum, n| sum + n }
-
-	# puts (@test/@lenders_needed.count)
-
-
-# puts @lenders_needed
-
-# puts @amount.last
-#this is not quite correct as it needs to add another 'needed' if the number is more than the one before and less than the noe after
-
-
-
+end
