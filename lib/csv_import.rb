510f05090875ee5280cd9e8d76808121d8dc27fd
TomGroombridge <groombridgetom@gmail.com>
2015-09-22 14:26:24 +0100

    fix the issue with not being able to have the correct amount of lenders assigned to a borrower

--- lib/csv_import.rb
+++ lib/csv_import.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 
 
-@amount_request = 1500
+@amount_request = 490
 @lenders = []
 CSV.foreach("../market.csv", headers: true) do |row|
   @lenders << {'name' => row[0], 'compound_interest' => row[1], 'amount' => row[2]}
@@ -11,31 +11,38 @@ end
 
 @lenders = @lenders.sort_by { |hsh| hsh['compound_interest'] }
 @amount_of_each_lender = @lenders.map{|h| h["amount"] }
+
+
 # puts @amount_of_each_lender.flatten.map(&:to_f)
 
 
 
 
-@i = 0
-@amount = @amount_of_each_lender.flatten.map(&:to_f).map {|feed| @i += feed }
-puts @amount
+@amount = @amount_of_each_lender.flatten.map(&:to_f)
+puts @amount.inspect
 
-if @amount_request > @amount.last
-	puts "sorry we can't offer you a loan"
-else
+# if @amount_request > @amount.last
+# 	puts "sorry we can't offer you a loan"
+# else
 	puts "congratulations we can offer you a loan"
 	@lenders_needed = []
-	@amount.each_with_index { |a, b| @lenders_needed << b unless a >= @amount_request}
+	@i = 0
+	@amount.each do |a|
+		unless @i >= @amount_request
+			@lenders_needed << @i
+			@i += a
+		end
+	# end
+
+
+
+end
 	puts @lenders_needed.count
-
-
-
 	@compound_interest = []
 	@lenders.first(@lenders_needed.count).each {|l| @compound_interest << l.values_at('compound_interest')}
 	@test = @compound_interest.flatten.map(&:to_f).inject { |sum, n| sum + n }
 
-	puts (@test/@lenders_needed.count)
-end
+	# puts (@test/@lenders_needed.count)
 
 
 # puts @lenders_needed
