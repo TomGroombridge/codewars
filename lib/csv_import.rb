77fc7f586dcfcffe25ab01948647e93cbfdc4b48
TomGroombridge <groombridgetom@gmail.com>
2015-09-22 14:06:40 +0100

    added notice if there is not enough funds to offer a quote

--- lib/csv_import.rb
+++ lib/csv_import.rb
@@ -1,21 +1,47 @@
 require 'csv'
 
+
+
+@amount_request = 2331
 @lenders = []
-@array = []
-CSV.foreach("../market.csv") do |row|
-  lender, rate, available = row
-  next if row[0] == "Lender"
-	@lenders << {'name' => row[0], 'compound_interest' => row[1], 'amount' => row[2]}
+CSV.foreach("../market.csv", headers: true) do |row|
+  @lenders << {'name' => row[0], 'compound_interest' => row[1], 'amount' => row[2]}
 end
-@lenders.each {|l| @array << l.values_at('amount')}
-@test = @array.flatten.map(&:to_i)
+
+
+@lenders = @lenders.sort_by { |hsh| hsh['compound_interest'] }
+@amount_of_each_lender = @lenders.map{|h| h["amount"] }
+# puts @amount_of_each_lender.flatten.map(&:to_f)
+
+
+
 
 @i = 0
-@lenders_needed = []
-@amount = @test.map {|feed| @i += feed }
-@amount.each do |a|
-   if a <= 1500
-    @lenders_needed <<  'needed'
-   end
+@amount = @amount_of_each_lender.flatten.map(&:to_f).map {|feed| @i += feed }
+puts @amount
+
+if @amount_request > @amount.last
+	puts "sorry we can't offer you a loan"
+else
+	puts "congratulations we can offer you a loan"
+	@lenders_needed = []
+	@amount.each { |a| @lenders_needed << a unless a >= @amount_request}
+
+
+
+
+	@compound_interest = []
+	@lenders.first(@lenders_needed.count).each {|l| @compound_interest << l.values_at('compound_interest')}
+	@test = @compound_interest.flatten.map(&:to_f).inject { |sum, n| sum + n }
+
+	# puts (@test/@lenders_needed.count)
 end
-puts @lenders.first(@lenders_needed.count)
+
+
+# puts @lenders_needed
+
+# puts @amount.last
+#this is not quite correct as it needs to add another 'needed' if the number is more than the one before and less than the noe after
+
+
+
