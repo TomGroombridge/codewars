78566704d887e121c8191138d2161a8ff2975d75
TomGroombridge <groombridgetom@gmail.com>
2015-09-24 12:25:53 +0100

    split more

--- lib/csv_import.rb
+++ lib/csv_import.rb
@@ -19,21 +19,30 @@ class Hello
 		@amount = @lenders.inject(0) {|sum, hash| sum + hash[:amount].to_f}
 		@lenders = @lenders.sort_by { |hsh| hsh[:compound_interest] }
 		@ordered_lenders = @lenders.map{|x| x[:amount].to_f}
+		check_for_loan
+	end
+
+	def check_for_loan
+		if @requested_amount > @amount
+			no_loan
+		else
+			lenders_needed
+		end
+	end
+
+	def lenders_needed
+		@lenders_needed = 0
+		@i = 0
+		@ordered_lenders.each do |a|
+			unless @i >= @requested_amount
+				@lenders_needed += 1
+				@i += a
+			end
+		end
 		set_loan
 	end
 
 	def set_loan
-		if @requested_amount > @amount
-			puts "sorry we can't offer you a loan"
-		else
-			@lenders_needed = 0
-			@i = 0
-			@ordered_lenders.each do |a|
-				unless @i >= @requested_amount
-					@lenders_needed += 1
-					@i += a
-				end
-			end
 			@total_compound_interest = []
 			@lenders.first(@lenders_needed).each {|l| @total_compound_interest << l.values_at(:compound_interest)}
 			@rate = (@total_compound_interest.flatten.map(&:to_f).inject(:+) / @lenders_needed).round(2)
@@ -49,7 +58,10 @@ class Hello
 			@total_repayments = @monthly_total
 			@monthly_repayments = (@monthly_total / 36)
 			puts "the amount requested was #{@requested_amount}, we can offer a loan at a rate of #{@rate.round(2)} with monthly repayments of #{@monthly_repayments.round(2)} with a total repayment of #{@total_repayments}"
-		end
+	end
+
+	def no_loan
+		puts "sorry we can't offer you a loan"
 	end
 
 end
